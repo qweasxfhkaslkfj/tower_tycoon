ad4342810a006400237bab805cec7d5f61d3718d
Siles9 <195898379+Siles9@users.noreply.github.com>
2026-05-08 10:33:59 +0300

    Update

--- tower tycoon/Assets/Scripts/Player/cameraFollow.cs
+++ tower tycoon/Assets/Scripts/Player/cameraFollow.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
+    public Transform target;
     public float smoothSpeed = 5f;
     public Vector3 offset;
 
     private void LateUpdate()
     {
-        Vector3 desiredPosition = player.position + offset;
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothPosition;
     }
