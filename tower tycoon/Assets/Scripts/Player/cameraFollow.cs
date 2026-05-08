a3e05fcc94e15439e62669ed6ae2fff9dc72da92
Pagan821 <arsenijantonov6.09@gmail.com>
2026-05-08 12:18:58 +0300

    added box collider 2D

--- tower tycoon/Assets/Scripts/Player/cameraFollow.cs
+++ tower tycoon/Assets/Scripts/Player/cameraFollow.cs
@@ -8,6 +8,11 @@ public class CameraFollow : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (player == null)
+        {
+            Debug.LogError("Player == null");
+            return;
+        }
         Vector3 desiredPosition = player.position + offset;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothPosition;
